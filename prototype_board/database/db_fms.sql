037dab497b9d97b7d5ce2d0bcfe9e5568b1489a7
skiedflakes <gagito0594@gmail.com>
2021-10-07 22:46:25 +0800

    Update db_fms.sql
    update db

--- prototype_board/database/db_fms.sql
+++ prototype_board/database/db_fms.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 02, 2021 at 11:13 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Generation Time: Oct 07, 2021 at 04:45 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,8 +42,32 @@ CREATE TABLE `files` (
 --
 
 INSERT INTO `files` (`f_id`, `user_id`, `filename`, `file_type`, `date_uploaded`, `feedback`, `file_path`) VALUES
-(1, 1, 'test', '.jpeg', '9-11-2021', 'test', 'asd'),
-(2, 2, 'test', 'test', 'test', 'test', 'test');
+(1, 0, 'test', 'testtes', '', 'test', 'test'),
+(2, 0, 'test', 'testtes', '', 'test', 'test'),
+(3, 0, 'test', 'testtes', '', 'test', 'test'),
+(4, 3, 'test', 'testtes', '', 'test', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(255) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_name`, `price`, `role`) VALUES
+(1, 'test5', '55', ''),
+(2, 'item2', '55', ''),
+(3, 'item3', '11', '');
 
 -- --------------------------------------------------------
 
@@ -79,7 +103,10 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `username`, `name`, `email`, `user_type`, `password`) VALUES
 (2, '790', '23', '1231@123123', 'user', 'dff54615aaf85d9b65f74f912b29e429'),
 (3, 'admin', 'admin', 'admin@asdasdw', 'admin', '827ccb0eea8a706c4c34a16891f84e7b'),
-(11, '123', 'yaps', 'yaps@yaps', 'user', '202cb962ac59075b964b07152d234b70');
+(11, '123', 'yaps', 'yaps@yaps', 'user', '202cb962ac59075b964b07152d234b70'),
+(13, '123', 'test', 'test@gmail.com', 'admin', '202cb962ac59075b964b07152d234b70'),
+(18, 'a', 'a', '', 'admin', 'd41d8cd98f00b204e9800998ecf8427e'),
+(19, '1', '1', '', 'admin', 'd41d8cd98f00b204e9800998ecf8427e');
 
 --
 -- Indexes for dumped tables
@@ -90,6 +117,12 @@ INSERT INTO `users` (`user_id`, `username`, `name`, `email`, `user_type`, `passw
 --
 ALTER TABLE `files`
   ADD PRIMARY KEY (`f_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
 
 --
 -- Indexes for table `users`
@@ -105,13 +138,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `f_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `f_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
