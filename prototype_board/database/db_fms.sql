84fc5db833fbc9ba61a556d81f33485b3b7d6b31
skiedflakes <gagito0594@gmail.com>
2021-10-28 16:51:50 +0800

    update
    update

--- prototype_board/database/db_fms.sql
+++ prototype_board/database/db_fms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2021 at 11:53 AM
+-- Generation Time: Oct 28, 2021 at 10:51 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -43,11 +43,12 @@ CREATE TABLE `files` (
 --
 
 INSERT INTO `files` (`f_id`, `folder_id`, `user_id`, `filename`, `file_type`, `date_uploaded`, `remarks`, `file_path`) VALUES
-(40, 0, 3, 'hahaha.png', 'png', '2021-10-24 18:15:13', 'test', '../uploads/hahaha.png'),
-(43, 0, 3, 'Screenshot (1).png', 'png', '2021-10-24 18:40:16', '', '../uploads/Screenshot (1).png'),
-(44, 0, 3, 'Screenshot (2).png', 'png', '2021-10-24 18:40:23', '', '../uploads/Screenshot (2).png'),
-(45, 0, 3, 'ramon.jpg', 'jpg', '2021-10-25 14:50:06', '', '../uploads/ramon.jpg'),
-(47, 0, 18, 'cpuz2.jpg', 'jpg', '2021-10-25 17:20:33', '', '../uploads/cpuz2.jpg');
+(7, 8, 3, 'cpuz3.jpg', 'jpg', '2021-10-28 16:34:08', 'test334', '../uploads/test34/cpuz3.jpg'),
+(8, 0, 3, 'cpuz3.jpg', 'jpg', '2021-10-28 16:34:59', '', '../uploads/cpuz3.jpg'),
+(9, 8, 3, 'cpuz2.jpg', 'jpg', '2021-10-28 16:35:13', '', '../uploads/test34/cpuz2.jpg'),
+(10, 8, 3, 'cpuz.jpg', 'jpg', '2021-10-28 16:35:31', 'haha', '../uploads/test34/cpuz.jpg'),
+(11, 9, 3, 'cpuz.jpg', 'jpg', '2021-10-28 16:50:03', '', '../uploads/test2/cpuz.jpg'),
+(12, 9, 3, 'cpuz2.jpg', 'jpg', '2021-10-28 16:50:08', '', '../uploads/test2/cpuz2.jpg');
 
 -- --------------------------------------------------------
 
@@ -57,16 +58,20 @@ INSERT INTO `files` (`f_id`, `folder_id`, `user_id`, `filename`, `file_type`, `d
 
 CREATE TABLE `folder` (
   `folder_id` int(255) NOT NULL,
+  `user_id` int(10) NOT NULL,
   `folder_path` varchar(255) NOT NULL,
-  `folder_name` varchar(255) NOT NULL
+  `folder_name` varchar(255) NOT NULL,
+  `desc` varchar(255) NOT NULL,
+  `date_created` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `folder`
 --
 
-INSERT INTO `folder` (`folder_id`, `folder_path`, `folder_name`) VALUES
-(1, '../uploads', 'Home');
+INSERT INTO `folder` (`folder_id`, `user_id`, `folder_path`, `folder_name`, `desc`, `date_created`) VALUES
+(8, 3, '../uploads/test34', 'test34', ' ', '2021-10-28 15:08:28'),
+(9, 3, '../uploads/test2', 'test2', ' ', '2021-10-28 16:49:54');
 
 -- --------------------------------------------------------
 
@@ -79,25 +84,6 @@ CREATE TABLE `share` (
   `user_id` int(11) NOT NULL,
   `file_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `share`
---
-
-INSERT INTO `share` (`share_id`, `user_id`, `file_id`) VALUES
-(7, 0, 0),
-(8, 0, 0),
-(9, 11, 0),
-(10, 11, 0),
-(12, 18, 0),
-(13, 13, 0),
-(15, 3, 0),
-(17, 18, 45),
-(18, 19, 45),
-(19, 18, 43),
-(20, 18, 40),
-(21, 3, 47),
-(22, 19, 47);
 
 -- --------------------------------------------------------
 
@@ -162,19 +148,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `f_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `f_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `folder`
 --
 ALTER TABLE `folder`
-  MODIFY `folder_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `folder_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `share`
 --
 ALTER TABLE `share`
-  MODIFY `share_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `share_id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
