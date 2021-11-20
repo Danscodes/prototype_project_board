e348eb025965b14bdeca2ae2be924d9cd150e193
skiedflakes <gagito0594@gmail.com>
2021-11-20 17:20:27 +0800

    updates
    updates

---
+++ db_connect_local.php
@@ -0,0 +1,5 @@
+<?php 
+$conn= new mysqli('localhost','root','','db_kstore')or die("Could not connect to mysql".mysqli_error($con));
+$db = mysqli_connect('localhost', 'root', '', 'db_kstore');
+
+?>
